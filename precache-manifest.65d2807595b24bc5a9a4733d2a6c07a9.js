5af382928de8b84246e9442941dfc912ef7ddd69
teran <teran4work@gmai.com>
2019-01-17 13:45:50 +0300

    deploy

---
+++ precache-manifest.65d2807595b24bc5a9a4733d2a6c07a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c29174c2565cce86d961",
+    "url": "/static/js/main.c29174c2.chunk.js"
+  },
+  {
+    "revision": "6c3b11d12d6aca8aebb2",
+    "url": "/static/js/1.6c3b11d1.chunk.js"
+  },
+  {
+    "revision": "c29174c2565cce86d961",
+    "url": "/static/css/main.76b3e9bf.chunk.css"
+  },
+  {
+    "revision": "0e148929b23be759b97363ff3b97928f",
+    "url": "/index.html"
+  }
+];
